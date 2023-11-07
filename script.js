81ed637e9252acc9c8c29f693dba8c94e3d0d080
Meraj Alam <124230839+meraj987@users.noreply.github.com>
2023-11-07 18:43:12 -0500

    Implement user input and reset functionality
    Implemented functionality for dynamically adjusting pixel art dimensions based on user input and added a reset option to clear the board

--- script.js
+++ script.js
@@ -1,21 +1,68 @@
-function pixels (number)
+
+
+
+function pixels (number, color)
 {
     
-    const board = document.querySelector(".board");
-
+let board = document.querySelector(".board");
+let squares = board.querySelectorAll("div");
+let lighter = document.querySelector(".lighter")
+squares.forEach((div) => div.remove());
 board.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
 board.style.gridTemplateRows = `repeat(${number}, 1fr)`
 let total = number * number 
 
 for (i =0; i < total; i++)
 {
-    const box = document.createElement('box')
+    let box = document.createElement('div')
     box.id = 'box'
-    box.style.backgroundColor = "green"
+    box.addEventListener("mouseover", () =>{
+        box.style.backgroundColor = "black"
+    })
+
+    lighter.addEventListener("click", function () {
+        box.addEventListener("mouseover", function (){
+            box.style.backgroundColor = "light" + color
+        })
+    })
+    box.style.backgroundColor = color
     board.insertAdjacentElement("beforeend", box)
 }
 
 }
+
+pixels(16, "white")
+
+
+
+const pixelInput = document.querySelector(".type");
+const pixelButton = document.querySelector(".value");
+const anyColor = document.querySelector(".anyColor");
+
+
+pixelButton.addEventListener("click", function () {
+    const number = parseInt(pixelInput.value, 10);
+
+    if (number >= 2 && number <= 100) {
+        const color = anyColor.value; 
+        pixels(number, color);
+    } else {
+        alert("Invalid input. Please enter a number between 2 and 100.");
+    }
+});
+
+ const reset = document.querySelector(".reset")
+ 
+
+ reset.addEventListener("click", ()=>{
+
+    let board = document.querySelector(".board");
+    let squares = board.querySelectorAll("div")
+    const color = "white"
+    anyColor.value = ""
+    pixelInput.value = 16
+    squares.forEach(div => div.style.background = "white")
+ })
 
 
 
